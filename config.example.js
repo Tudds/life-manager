88599b79cb551d6cf7fc58c73b80a736114b6807
Đinh Tú <dinhtu.hh@gmail.com>
2026-01-21 19:14:36 +0700

    Add config.js from example configuration

--- config.example.js
+++
@@ -1,9 +0,0 @@
-// Configuration Template
-// Rename this file to config.js and fill in your values
-const CONFIG = {
-    CLIENT_ID: 'YOUR_CLIENT_ID_HERE',
-    API_KEY: 'YOUR_API_KEY_HERE',
-    SPREADSHEET_ID: 'YOUR_SPREADSHEET_ID_HERE',
-    SCOPES: 'https://www.googleapis.com/auth/spreadsheets',
-    DISCOVERY_DOCS: ['https://sheets.googleapis.com/$discovery/rest?version=v4']
-};
---
+++ config.js
@@ -0,0 +1,9 @@
+// Configuration - Private Credentials
+// DO NOT COMMIT THIS FILE TO GIT
+const CONFIG = {
+    CLIENT_ID: '63508353735-qk6vlh3r1ioc5r3ibdo78ef4sda0k1c8.apps.googleusercontent.com',
+    API_KEY: 'AIzaSyDUybY8UbY1ikVjQ0EdkFtN9gtoHxJrHMA',
+    SPREADSHEET_ID: '1WFaBacFPOlwExLrTYfRvNlCioEHxv_2-cM7xxuCwAFw',
+    SCOPES: 'https://www.googleapis.com/auth/spreadsheets',
+    DISCOVERY_DOCS: ['https://sheets.googleapis.com/$discovery/rest?version=v4']
+};
